0c5ab120f7cf7bec60f841a5649c31d6f9c327c3
seunggyun-jeong <jskyun98@gmail.com>
2022-08-18 11:40:53 +0900

    [ADD] 버튼 그림자 추가, ReportView로 NaviagtionLink 연결

---
+++ ENG/Views/ FacilityListViews/FacilityDetailViews/ReportView.swift
@@ -0,0 +1,20 @@
+//
+//  ReportView.swift
+//  ENG
+//
+//  Created by 정승균 on 2022/08/18.
+//
+
+import SwiftUI
+
+struct ReportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReportView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReportView()
+    }
+}
