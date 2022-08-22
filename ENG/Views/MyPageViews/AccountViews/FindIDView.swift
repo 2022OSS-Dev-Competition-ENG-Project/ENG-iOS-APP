f88fd36d76ebd109728f4ebc7b0fd8ed664bf9d2
seunggyun-jeong <jskyun98@gmail.com>
2022-08-22 16:38:55 +0900

    [FEAT] ID/PW 찾기 뷰 UI 구현

--- ENG/Views/MyPageViews/AccountViews/FindIDView.swift
+++ ENG/Views/MyPageViews/AccountViews/FindIDView.swift
@@ -8,13 +8,119 @@
 import SwiftUI
 
 struct FindIDView: View {
+    
+    @State var nameTextField: String = ""
+    @State var phoneNumberTextField: String = ""
+    
+    @State var foundedId: String = ""
+    @State var isFoundId: Bool = false
+    @State var isCanNotFindId: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView
+            
+            InputView
+        }
+        .onTapGesture {
+            hideKeyboard()
+        }
+        .navigationTitle("ID 찾기")
+        .navigationBarTitleDisplayMode(.inline)
+        .alert("아이디 찾기", isPresented: $isFoundId) {
+            Button("확인", action: { })
+        } message: {
+            Text("회원님의 아이디는 \(foundedId)입니다.")
+        }
+        .alert("아이디 찾기 실패", isPresented: $isCanNotFindId) {
+            Button("확인", action: { })
+        } message: {
+            Text("아이디 찾기에 실패하였습니다. 다시 한 번 확인해주세요.")
+        }
+    }
+    
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
 struct FindIDView_Previews: PreviewProvider {
     static var previews: some View {
-        FindIDView()
+        NavigationView {
+            FindIDView()
+        }
     }
+}
+
+extension FindIDView {
+    private var HeaderView: some View {
+        VStack {
+            Image("Logo")
+            Text("아이디를 잊어버리셨나요?")
+                .font(.custom(Font.theme.mainFontBold, size: 15))
+                .foregroundColor(.theme.accent)
+                .padding(.bottom, 33)
+        }
+    }
+    
+    private var InputView: some View {
+        VStack {
+            ZStack {
+                TextField("이름을 입력하세요.", text: $nameTextField)
+                    .customTextField(padding: 13)
+                    .frame(width: 288, alignment: .center)
+                    .font(.custom(Font.theme.mainFont, size: 15))
+                HStack {
+                    Spacer()
+
+                    Image(systemName: "greetingcard")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20, alignment: .center)
+                        .padding(.trailing, 10)
+                    
+
+                }
+                .frame(width: 288)
+
+            }
+            
+            ZStack {
+                SecureField("전화번호를 입력하세요. ('-' 제외)", text: $phoneNumberTextField)
+                    .customTextField(padding: 13)
+                    .frame(width: 288, alignment: .center)
+                    .font(.custom(Font.theme.mainFont, size: 15))
+                HStack {
+                    Spacer()
+
+                    Image(systemName: "candybarphone")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20, alignment: .center)
+                        .padding(.trailing, 10)
+                }
+                .frame(width: 288)
+            }
+            
+            Button {
+                findID()
+            } label: {
+                Text("아이디 찾기")
+                    .frame(width: 288, height: 40, alignment: .center)
+                    .background(Color.theme.accent)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                    .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 4)
+            }
+            .padding(.top, 30)
+
+        }
+    }
+    
+    private func findID() {
+        isFoundId = true
+        foundedId = "nefniei@icslab.com"
+        print("아이디 찾기 성공")
+    }
+    
 }
--- ENG/Views/MyPageViews/AccountViews/FindPasswordView.swift
+++ ENG/Views/MyPageViews/AccountViews/FindPasswordView.swift
@@ -8,13 +8,111 @@
 import SwiftUI
 
 struct FindPasswordView: View {
+    
+    @State var emailTextField: String = ""
+    @State var nameTextField: String = ""
+    
+    @State var isAvaliable: Bool = false
+    @State var isError: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView
+            
+            InputView
+        }
+        .navigationTitle("비밀번호 찾기")
+        .navigationBarTitleDisplayMode(.inline)
+        .alert("비밀번호 초기화 성공", isPresented: $isAvaliable) {
+            Button("확인", action: { })
+        } message: {
+            Text("입력하신 \(emailTextField)로 초기화된 비밀번호를 전송하였습니다.")
+        }
+        .alert("입력 오류", isPresented: $isError) {
+            Button("확인", action: { })
+        } message: {
+            Text("입력하신 정보가 없거나, 일치하지 않습니다.\n 다시 한 번 확인해주세요.")
+        }
     }
 }
 
 struct FindPasswordView_Previews: PreviewProvider {
     static var previews: some View {
-        FindPasswordView()
+        NavigationView {
+            FindPasswordView()
+        }
     }
+}
+
+extension FindPasswordView {
+    private var HeaderView: some View {
+        VStack {
+            Image("Logo")
+            Text("비밀번호를 잊으셨나요?")
+                .font(.custom(Font.theme.mainFontBold, size: 15))
+                .foregroundColor(.theme.accent)
+                .padding(.bottom, 33)
+        }
+    }
+    
+    private var InputView: some View {
+        VStack {
+            ZStack {
+                TextField("이메일(아이디)을 입력하세요.", text: $emailTextField)
+                    .customTextField(padding: 13)
+                    .frame(width: 288, alignment: .center)
+                    .font(.custom(Font.theme.mainFont, size: 15))
+                HStack {
+                    Spacer()
+
+                    Image(systemName: "mail")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20, alignment: .center)
+                        .padding(.trailing, 10)
+                    
+
+                }
+                .frame(width: 288)
+
+            }
+            
+            ZStack {
+                SecureField("이름을 입력하세요.", text: $nameTextField)
+                    .customTextField(padding: 13)
+                    .frame(width: 288, alignment: .center)
+                    .font(.custom(Font.theme.mainFont, size: 15))
+                HStack {
+                    Spacer()
+
+                    Image(systemName: "greetingcard")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20, alignment: .center)
+                        .padding(.trailing, 10)
+                }
+                .frame(width: 288)
+            }
+            
+            Button {
+                findPW()
+            } label: {
+                Text("비밀번호 찾기")
+                    .frame(width: 288, height: 40, alignment: .center)
+                    .background(Color.theme.accent)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+                    .shadow(color: .black.opacity(0.25), radius: 4, x: 0, y: 4)
+            }
+            .padding(.top, 30)
+
+        }
+    }
+    
+    private func findPW() {
+        isAvaliable = true
+//        isError = true
+        print("비밀번호 찾기 완료")
+    }
+    
 }
