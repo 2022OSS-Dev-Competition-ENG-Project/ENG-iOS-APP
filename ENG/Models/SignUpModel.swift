acf839fb0ffb5225fd11792345558d8aefe96de6
seunggyun-jeong <jskyun98@gmail.com>
2022-08-23 22:48:13 +0900

    [ADD] SignUp 모델 및 뷰모델 추가

---
+++ ENG/Models/SignUpModel.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfoModel.swift
+//  ENG
+//
+//  Created by 정승균 on 2022/08/23.
+//
+
+import Foundation
+
+struct SignUpModel: Codable {
+    let userEmail: String
+    let userPassword: String
+    let userName: String
+    let userNickname: String
+    let userPhoneNum: String
+}
---
+++ ENG/ViewModels/SignUpViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SignUpViewModel.swift
+//  ENG
+//
+//  Created by 정승균 on 2022/08/23.
+//
+
+import Foundation
