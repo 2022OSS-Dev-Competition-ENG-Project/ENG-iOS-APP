06fa34bb9971bf8a8e7d4ec5bae2bebc553629d5
seunggyun-jeong <jskyun98@gmail.com>
2022-08-24 13:28:43 +0900

    [ADD] LoginModel 구현

---
+++ ENG/Models/LoginModel.swift
@@ -0,0 +1,33 @@
+//
+//  LoginModel.swift
+//  ENG
+//
+//  Created by 정승균 on 2022/08/24.
+//
+
+import Foundation
+
+// REQUEST DATA
+/*
+ {
+    "userEmail" : "cb7856@naver.com",
+    "userPassword" : "1234"
+ }
+*/
+struct LoginRequest: Codable {
+    let userEmail: String
+    let userPassword: String
+}
+
+// RESPONSE DATA
+/*
+ {
+     "token" : eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJjYjc4NTZAbmF2ZXIuY29tIiwibmFtZSI6ImNiNzg1NkBuYXZlci5jb20iLCJleHAiOjE2NjEzMTY4MDh9.bitRUU3WN15TZ-ujcVQfW_1-7HpkYScUuNDnylU12s8
+ }
+ */
+
+struct LoginResponse: Codable {
+    let token: String
+}
+
+
