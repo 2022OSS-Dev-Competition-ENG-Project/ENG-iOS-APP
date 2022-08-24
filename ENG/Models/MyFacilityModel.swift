7ef83d6c50c527ec05c8f872f178846feed4f113
seunggyun-jeong <jskyun98@gmail.com>
2022-08-24 10:14:05 +0900

    [ADD] MyFaciltyModel 생성

---
+++ ENG/Models/MyFacilityModel.swift
@@ -0,0 +1,32 @@
+//
+//  MyFacilityModel.swift
+//  ENG
+//
+//  Created by 정승균 on 2022/08/24.
+//
+
+import Foundation
+/*
+ JSON :
+ [
+     {
+         "userUuid": "343890ca-4bab-4424-9a8d-5f8f90533b77",
+         "facilityAddress": "경상북도 경산시 하양읍 하양로 13-13",
+         "userFacility": "98097998-95f6-4a04-9ab6-d71b672e2782",
+         "facilityName": "대구가톨릭대학교"
+     },
+     {
+         "userUuid": "343890ca-4bab-4424-9a8d-5f8f90533b77",
+         "facilityAddress": "경상북도 경산시 하양읍 대경로 669-5",
+         "userFacility": "b31a1bfc-80de-41c6-bcaa-a08567c230d8",
+         "facilityName": "하양 한울타리"
+     }
+ ]
+*/
+
+struct MyFacilityModel: Codable {
+    let userUuid: String
+    let facilityAddress: String
+    let userFacility: String
+    let facilityName: String
+}
