8acdfc0baf3dee7c055e2edcd17af934ff816570
seunggyun-jeong <jskyun98@gmail.com>
2022-08-24 21:43:50 +0900

    [FEAT] 내가 사용할 시설물 등록 기능 구현

--- ENG/Views/ FacilityListViews/FacilityDetailViews/QRScanner/QRCodeScanner.swift
+++ ENG/Views/ FacilityListViews/FacilityDetailViews/QRScanner/QRCodeScanner.swift
@@ -6,8 +6,11 @@
 //
 
 import SwiftUI
+import Combine
 
 struct QRCodeScanner: View {
+    
+    var NM = NetworkManager.shared
 
     @State private var cardNumber: String = "//"
     @State private var isCardNumberChange: Bool = false
@@ -25,11 +28,49 @@ struct QRCodeScanner: View {
                 QRCameraCell(cardNumber: $cardNumber, isCardNumberChange: $isCardNumberChange)
             }
             .alert("스캔 완료", isPresented: $isCardNumberChange) {
-                Button("OK") { }
+                Button("OK") { RegisterFacility(data: FacilityResgistrationModel(userUuid: UserDefaults.standard.string(forKey: "loginToken") ?? "", userFacility: qrComponents[0]))}
                 Button("취소") { cardNumber = "/" }
             } message: {
                 Text("\(qrComponents[1]) (\(qrComponents[2]))가 맞나요?")
             }
     }
+    
+    private func RegisterFacility(data: FacilityResgistrationModel) {
+        var cancellables = Set<AnyCancellable>()
+        
+        guard let upLoadData = try? JSONEncoder().encode(data) else { return }
+        print(String(decoding: upLoadData, as: UTF8.self))
+        let request: URLRequest
+        
+        do {
+            request = try NM.makePostRequest(api: "/api/facility/join", data: upLoadData, ip: NM.facilityIp)
+        } catch(let error) {
+            print("error: \(error)")
+            return
+        }
+        
+        
+        URLSession.shared.dataTaskPublisher(for: request)
+            .subscribe(on: DispatchQueue.global(qos: .background))
+            .receive(on: DispatchQueue.main)
+            .map() {
+                $0.response as? HTTPURLResponse
+            }
+            .sink { completion in
+                print(completion)
+            } receiveValue: { data in
+                guard let statusCode = data?.statusCode else { return }
+                print("시설물 등록 statusCode == \(statusCode)")
+                if statusCode == 200 {
+                    print("시설물 등록 완료")
+                } else if statusCode == 409 {
+                    print("이미 등록 된 시설물")
+                } else if statusCode == 404 {
+                    print("없는 시설물")
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
 }
 
