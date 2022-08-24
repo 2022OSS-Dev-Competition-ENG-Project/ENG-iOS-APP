011cb2124229bf4314679d671556a26fbba03d5f
seunggyun-jeong <jskyun98@gmail.com>
2022-08-24 14:23:06 +0900

    [ADD] NetworkManager를 이용하여 네트워킹 작업을 할 수 있도록 수정

--- ENG/Network/NetworkResources.swift
+++ ENG/Network/NetworkResources.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class NetworkManager {
+    
+    // NetworkManager를 싱클톤 패턴으로 운용
+    static let shared = {
+        return NetworkManager()
+    }()
+    
+    private init() {
+        
+    }
+    
+    let facilityIp: String = "http://203.250.32.29:2200"
+    let userIp: String = "http://203.250.32.29:2201"
+
+    func makePostRequest(api: String, data: Data = Data(), ip: String) throws -> URLRequest {
+        guard let url = URL(string: ip + api) else {
+            throw URLError(.badURL)
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpBody = data
+        
+        return urlRequest
+    }
+
+}
--- ENG/ViewModels/MyFacilityViewModel.swift
+++ ENG/ViewModels/MyFacilityViewModel.swift
@@ -8,21 +8,22 @@
 import Foundation
 import Combine
 
-let facilityIp: String = "http://203.250.32.29:2200"
+
 
 class MyFaciltyViewModel: ObservableObject {
     
-    @Published var MyFacilities: [MyFacilityModel] = []
-    
+    let NM = NetworkManager.shared
     var cancellables = Set<AnyCancellable>()
     
+    @Published var MyFacilities: [MyFacilityModel] = []
+
     init() {
         getFacilities(userUUID: "343890ca-4bab-4424-9a8d-5f8f90533b77")
     }
     
     // get facilities
     private func getFacilities(userUUID: String) {
-        guard let url = URL(string: facilityIp + "/api/facility/" + userUUID + "/list") else { return }
+        guard let url = URL(string: NM.facilityIp + "/api/facility/" + userUUID + "/list") else { return }
 
         URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .background))
