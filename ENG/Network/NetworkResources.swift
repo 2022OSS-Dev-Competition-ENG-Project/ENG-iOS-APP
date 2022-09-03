df16248baa8c47a996e9263178c85f6adcc2f0f8
seunggyun-jeong <jskyun98@gmail.com>
2022-09-03 20:36:27 +0900

    [FEAT] Form-Data로 데이터 전송 메서드 구현 및 신고 뷰 수정

--- ENG/Network/NetworkResources.swift
+++ ENG/Network/NetworkResources.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 class NetworkManager {
     
@@ -33,5 +34,43 @@ class NetworkManager {
         
         return urlRequest
     }
+    
+    // 폼 데이터 보내기
+    func uploadImage(paramName: String, fileName: String, image: UIImage) {
+        let url = URL(string: "http://192.168.0.53:2201/api/user-service/SaveProfileImage/0f797583-f9dd-4ec3-bb59-39d4cf862ed1")
+        
+        // generate boundary string using a unique per-app string
+        let boundary = UUID().uuidString
 
+        let session = URLSession.shared
+
+        // Set the URLRequest to POST and to the specified URL
+        var urlRequest = URLRequest(url: url!)
+        urlRequest.httpMethod = "POST"
+
+        // Set Content-Type Header to multipart/form-data, this is equivalent to submitting form data with file upload in a web browser
+        // And the boundary is also set here
+        urlRequest.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+        var data = Data()
+
+        // Add the image data to the raw http request data
+        data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+        data.append("Content-Disposition: form-data; name=\"\("images")\"; filename=\"\(fileName)\"\r\n".data(using: .utf8)!)
+        data.append("Content-Type: image/png\r\n\r\n".data(using: .utf8)!)
+        data.append(image.pngData()!)
+
+        data.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+//        print("-----> data \(String(decoding: data, as: UTF8.self))")
+        // Send a POST request to the URL, with the data we created earlier
+        session.uploadTask(with: urlRequest, from: data, completionHandler: { responseData, response, error in
+            if error == nil {
+                let jsonData = try? JSONSerialization.jsonObject(with: responseData!, options: .allowFragments)
+                if let json = jsonData as? [String: Any] {
+                    print(json)
+                }
+            }
+        }).resume()
+    }
+    
 }
--- ENG/Views/ FacilityListViews/FacilityDetailViews/Report/ReportView.swift
+++ ENG/Views/ FacilityListViews/FacilityDetailViews/Report/ReportView.swift
@@ -18,6 +18,10 @@ struct ReportView: View {
     @State var placeHolderTextField: String = "글 내용을 입력하세요. (500자 이내)"
     @FocusState var reportContentTextFieldFocused: Bool
     
+    @State var uploadImage: UIImage = UIImage(systemName: "circle") ?? UIImage()
+    
+    let NM = NetworkManager.shared
+    
     var body: some View {
         VStack(spacing: 19) {
             TextField("제목을 입력해주세요.", text: $reportTitleTextField)
@@ -60,33 +64,38 @@ struct ReportView: View {
             
             
             HStack(spacing: 44) {
-                // 갤러리 및 카메라에 접근하여 업로드 가능하도록.
-                image[0]?
-                    .resizable()
-                    .frame(width: 90, alignment: .center)
-                    .onTapGesture {
-                        image[0] = nil
-                    }
                 
-                image[1]?
-                    .resizable()
-                    .frame(width: 90, alignment: .center)
-                    .onTapGesture {
-                        image[1] = nil
+                ForEach(0..<3) { index in
+                    ZStack(alignment: .topTrailing) {
+                        image[index]?
+                            .resizable()
+                            .frame(width: 90, alignment: .center)
+                            .overlay(Rectangle()
+                                .fill(Color.black.opacity(0.25))
+                            )
+                        
+                        Button {
+                            // 이미지 삭제
+                            image[index] = nil
+                        } label: {
+                            Image(systemName: "x.circle.fill")
+                                .padding(.all, 5)
+                                .foregroundColor(.white)
+                                .opacity(image[index] != nil ? 1 : 0)
+                        }
+
                     }
+                    .frame(width: 90, height: 90, alignment: .center)
+                    .border(.black, width: 1)
+                }
                 
-                image[2]?
-                    .resizable()
-                    .frame(width: 90, alignment: .center)
-                    .onTapGesture {
-                        image[2] = nil
-                    }
             }
             .frame(width: UIScreen.main.bounds.width - 32, height: 90, alignment: .center)
             .sheet(isPresented: $showImagePicker) {
                 ImagePicker(sourceType: .photoLibrary) { image in
                     if self.image[0] == nil {
                         self.image[0] = Image(uiImage: image)
+                        self.uploadImage = image
                     }
                     else if self.image[1] == nil {
                         self.image[1] = Image(uiImage: image)
@@ -126,6 +135,7 @@ struct ReportView: View {
         .navigationBarTitleDisplayMode(.inline)
         .navigationBarItems(trailing: Button(action: {
             // 등록 기능
+            NM.uploadImage(paramName: "multipartFile", fileName: "pic.png", image: uploadImage)
         }, label: {
             Text("등록")
                 .font(.custom(Font.theme.mainFontBold, size: 16))
